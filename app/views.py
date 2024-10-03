b08319c8a6f7a63698350129beaac9aada3b2739
Rhushikeshnandodkar <rhushikeshnandodkar2003@gmail.com>
2024-10-03 07:14:25 -0700

    details page added with recommendations

--- app/views.py
+++ app/views.py
@@ -6,17 +6,21 @@ import os
 from django.conf import settings
 # Create your views here.
 def index(request):
-    if request.method == 'POST':
-        places_pickle = os.path.join(settings.MEDIA_ROOT, 'movies.pkl')
-        similarity_pickle = os.path.join(settings.MEDIA_ROOT, 'similarity.pkl')
+    places = PlaceMode.objects.all()
+    return render(request, 'index.html', {"places" : places})
+
+def details(request, pk):
+    place = PlaceMode.objects.get(id=pk)
+    places_pickle = os.path.join(settings.MEDIA_ROOT, 'movies.pkl')
+    similarity_pickle = os.path.join(settings.MEDIA_ROOT, 'similarity.pkl')
         
-        place_dict = pickle.load(open(places_pickle, 'rb'))
-        similarity = pickle.load(open(similarity_pickle, 'rb'))
-        name = request.POST.get('name')
-        place_index = place_dict[place_dict['Name'] == name].index[0]
-        distances = similarity[place_index]
-        places_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x : x[1])[1:15]
-        recommended_places = []
-        for i in places_list:
-            recommended_places.append(PlaceMode.objects.get(id=i[0]))
-    return render(request, 'index.html', {"places" : recommended_places})
+    place_dict = pickle.load(open(places_pickle, 'rb'))
+    similarity = pickle.load(open(similarity_pickle, 'rb'))
+    name = place.Name
+    place_index = place_dict[place_dict['Name'] == name].index[0]
+    distances = similarity[place_index]
+    places_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x : x[1])[1:15]
+    recommended_places = []
+    for i in places_list:
+        recommended_places.append(PlaceMode.objects.get(id=i[0]))
+    return render(request, 'details.html', {"rec_places" : recommended_places, 'single_place' : place})
